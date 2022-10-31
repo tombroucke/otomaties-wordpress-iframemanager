83717ccb3effd61f891987e5112d71bbb6d3ed05
Tom Broucke <tom@tombroucke.be>
2022-10-31 17:52:24 +0100

    chore(bump): Bump version to 1.0.1

--- otomaties-wordpress-iframemanager.php
+++ otomaties-wordpress-iframemanager.php
@@ -5,7 +5,7 @@ namespace Otomaties\OtomatiesWordpressIframemanager;
 /**
  * Plugin Name:       Otomaties WordPress Iframemanager
  * Description:       GDPR friendly iframe manager
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Tom Broucke
  * Author URI:        https://tombroucke.be/
  * License:           GPL-2.0+
